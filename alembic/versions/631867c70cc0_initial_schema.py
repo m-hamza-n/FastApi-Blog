f4c8448438bc4c54d5407f8134a7c48a4616a2d1
m-hamza-n <197356178+m-hamza-n@users.noreply.github.com>
2026-07-06 17:16:26 +0500

    Remove residual artifact from initial migration

--- alembic/versions/631867c70cc0_initial_schema.py
+++ alembic/versions/631867c70cc0_initial_schema.py
@@ -68,4 +68,3 @@ def downgrade() -> None:
     op.drop_index(op.f('ix_users_id'), table_name='users')
     op.drop_table('users')
     # ### end Alembic commands ###
-DB_URL = os.getenv('DATABASE_URL')
